ba8e09f412de9cbbd1c39e979354839271e680d1
aurelienog <aurelienog@gmail.com>
2026-02-28 13:15:17 +0100

    Delete ex5 directory

--- ex5/ft_count_harvest_iterative.py
+++
@@ -1,7 +0,0 @@
-def ft_count_harvest_iterative():
-    remainder = input("Days until harvest: ")
-    i = 1
-    while (i <= int(remainder)):
-        print("Day", i)
-        i += 1
-    print("Harvest time!")
--- ex5/ft_count_harvest_recursive.py
+++
@@ -1,9 +0,0 @@
-def ft_count_harvest_recursive(i=1, days=0):
-    if (days == 0):
-        days = int(input("Days until harvest: "))
-    if (i > days):
-        print("Harvest time!")
-        return
-    else:
-        print("Day", i)
-        ft_count_harvest_recursive(i + 1, days)
