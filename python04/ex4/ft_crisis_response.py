4f4b2a61f13bd66d2ff9d9b377b546069d3f66cf
Aurelie Nogueira <aurelienog@gmail.com>
2026-03-13 19:59:07 +0100

    refactor crisis_response

--- python04/ex4/ft_crisis_response.py
+++ python04/ex4/ft_crisis_response.py
@@ -1,24 +1,34 @@
 #!/usr/bin/env python3
 
-def report_crisis() -> None:
-    print("CRISIS ALERT: Attempting access to 'lost_archive.txt'...")
-    try:
-        with open("new_discovery.txt", "w") as file:
-            file.write(
-                "[ENTRY 001] New quantum algorithm discovered"
-                "\n[ENTRY 002] Efficiency increased by 347%"
-                "\n[ENTRY 003] Archived by Data Archivist trainee"
-                )
-            print("Data inscription complete. Storage unit sealed.")
-            print("Archive 'new_discovery.txt' ready for long-term",
-                  "preservation.")
-    except PermissionError:
-        print("ERROR writing file: Permission denied")
+
+def handle_crisis(documents: list) -> None:
+    for doc in documents:
+        try:
+            with open(doc, "r") as file:
+                content = file.read()
+            print(f"ROUTINE ACCESS: Attempting access to '{doc}'...")
+            print(f"SUCCESS: Archive recovered - ``{content}''")
+            print("STATUS: Normal operations resumed\n")
+        except FileNotFoundError:
+            print(f"CRISIS ALERT: Attempting access to '{doc}'...")
+            print("RESPONSE: Archive not found in storage matrix")
+            print("STATUS: Crisis handled, system stable\n")
+        except PermissionError:
+            print(f"CRISIS ALERT: Attempting access to '{doc}'...")
+            print("RESPONSE: Security protocols deny access")
+            print("STATUS: Crisis handled, security maintained\n")
+        except Exception:
+            print(f"CRISIS ALERT: Attempting access to '{doc}'...")
+            print("RESPONSE: Unexpected system anomaly")
+            print("STATUS: Crisis handled, system stable\n")
 
 
 def main() -> None:
-    print("=== CYBER ARCHIVES - CRISIS RESPONSE SYSTEM ===")
-    report_crisis()
+    documents = ["lost_archive.txt", "classified_vault.txt",
+                 "standard_archive.txt"]
+    print("=== CYBER ARCHIVES - CRISIS RESPONSE SYSTEM ===\n")
+    handle_crisis(documents)
+    print("All crisis scenarios handled successfully. Archives secure.")
 
 
 if __name__ == "__main__":
