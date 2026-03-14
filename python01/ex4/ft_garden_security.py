11c3b85fabf624405d46ac22122f225897f3923b
Aurelie Nogueira <aurelienog@gmail.com>
2026-03-14 23:29:15 +0100

    update encapsulation, now protected

--- python01/ex4/ft_garden_security.py
+++ python01/ex4/ft_garden_security.py
@@ -3,15 +3,15 @@
 class SecurePlant:
 
     def __init__(self: "SecurePlant", name: str) -> None:
-        self.__name = name
-        self.__height = 0
-        self.__age = 0
+        self._name = name
+        self._height = 0
+        self._age = 0
         print(f"Plant created: {name}")
 
     def set_height(self: "SecurePlant", height: int) -> None:
         if height >= 0:
-            self.__height = height
-            print(f"Height updated: {self.__height}cm [OK]")
+            self._height = height
+            print(f"Height updated: {self._height}cm [OK]")
         else:
             print(f"Invalid operation attempted: height {height}cm",
                   "[REJECTED]")
@@ -19,21 +19,21 @@ class SecurePlant:
 
     def set_age(self: "SecurePlant", age: int) -> None:
         if age >= 0:
-            self.__age = age
-            print(f"Age updated: {self.__age} days [OK]")
+            self._age = age
+            print(f"Age updated: {self._age} days [OK]")
         else:
             print(f"Invalid operation attempted: age {age} days",
                   "[REJECTED]")
             print("Security: Negative age rejected")
 
     def get_height(self: "SecurePlant") -> int:
-        return self.__height
+        return self._height
 
     def get_age(self: "SecurePlant") -> int:
-        return self.__age
+        return self._age
 
     def get_info(self: "SecurePlant") -> None:
-        print(f"Current plant: {self.__name} ({self.__height}cm, {self.__age}",
+        print(f"Current plant: {self._name} ({self._height}cm, {self._age}",
               "days)")
 
 
