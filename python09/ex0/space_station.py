f48764137f61dbb02a578ba7e5e0f19619966c27
Aurelie Nogueira <aurelienog@gmail.com>
2026-04-14 14:38:51 +0200

    update space_station.py

--- python09/ex0/space_station.py
+++ python09/ex0/space_station.py
@@ -33,31 +33,55 @@ class SpaceStation(BaseModel):
     )
 
 
+def print_station(station: SpaceStation) -> None:
+    print("Valid station created:")
+    print(f"ID: {station.station_id}")
+    print(f"Name: {station.name}")
+    print(f"Crew: {station.crew_size}")
+    print(f"Power: {station.power_level}")
+    print(f"Oxygen: {station.oxygen_level}")
+    if station.is_operational:
+        print("Status: Operational")
+    else:
+        print("Status: Not Operational")
+
+
 def main() -> None:
     print("Space Station Data Validation")
-    print("========================================")
 
-    try:
-        station = SpaceStation(
-            station_id="SFSF",
-            name="Alpha",
-            crew_size=5,
-            power_level=80.5,
-            oxygen_level=95.0,
-            last_maintenance=datetime.now()
-        )
-        for key, value in station.model_dump().items():
-            if value is not None:
-                if key == "last_maintenance":
-                    value = value.strftime("%d/%m/%Y %H:%M")
-                print(f"{key}: {value}")
+    stations: list[dict] = [
+        {
+            "station_id": "LGW125",
+            "name": "Titan Mining Outpost",
+            "crew_size": 6,
+            "power_level": 76.4,
+            "oxygen_level": 95.5,
+            "last_maintenance": "2023-07-11T00:00:00",
+            "is_operational": True,
+            "notes": None
+        }, {
+            "station_id": "TOOLONG123456",
+            "name": "Test Station",
+            "crew_size": 25,
+            "power_level": 85.0,
+            "oxygen_level": 92.0,
+            "last_maintenance": "2024-01-15T10:30:00",
+            "is_operational": True
+        },
+    ]
+    for s in stations:
+        print("========================================")
+        try:
+            station = SpaceStation(**s)
+            print_station(station)
 
-    except ValidationError as error:
-        for err in error.errors():
-            field = ".".join(str(x) for x in err["loc"])
-            message = err["msg"]
-            print(f"{field}: {message.lower()}")
-    print("========================================")
+        except ValidationError as error:
+            print("Expected validation error:")
+            for err in error.errors():
+                field = ".".join(str(x) for x in err["loc"])
+                message = err["msg"]
+                print(f"{field}: {message.lower()}")
+        print("========================================")
 
 
 if __name__ == "__main__":
